d28554337496d81928f7d30382b4168ee411cb22
Tyrone Shine <tyronewshine@gmail.com>
2020-11-12 14:02:01 -0500

    Made the quantity button functional and crossbrowser ready

---
+++ src/js/script.js
@@ -0,0 +1,34 @@
+(function () {
+	'use strict'
+
+	// BEGIN: Quantity selector
+    const quantityButtons = document.querySelectorAll('[data-quantity-change]')
+    const quantity = document.querySelector('.product-details__number')
+    // The quantity amount is set as a let since it needs to be updated
+    let quantityAmount = 0
+    // I am setting the value here so that the input fields info
+    // doesn't carry over from a page refresh
+    quantity.value = quantityAmount
+
+    quantityButtons.forEach( button => {
+    	const changeQuantity = button.dataset.quantityChange
+
+    	button.addEventListener('click', e => {
+
+    		if (changeQuantity === 'up') {
+    			quantityAmount++
+    			quantity.value = quantityAmount
+    		} 
+
+    		if (changeQuantity === 'down' && quantityAmount >= 1) {
+    			// Added a limit on how far down the quantity can go to match the input
+    			// because giving a negative quantity makes little sense
+    			quantityAmount--
+				quantity.value = quantityAmount
+    		}
+    	})
+    })
+    // END: Quantity selector
+
+
+})()
